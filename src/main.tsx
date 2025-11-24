c91a1bef6f8626877f6f1dc036f4880de46ffeb3
edwlee <edwlee@student.ethz.ch>
2025-11-24 13:50:48 -0800

    fix: resolve remaining ESLint and TypeScript CI errors

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import { App } from './app/App';
 import './styles/globals.css';
 import './i18n';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
